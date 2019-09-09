20bed33cc8144e53d3a1af680723c82d9d1de0d4
Christopher Fredén <me@icetan.org>
2019-09-09 11:39:56 +0200

    Move input from module config to argument

--- examples/network-2-nodes.nix
+++ examples/network-2-nodes.nix
@@ -1,16 +1,16 @@
 let
   common = import ./common.nix;
-in {
-  "node-1" = { config, ... }: {
+in input: {
+  "node-1" = { config, input, ... }: {
     require = [ (common.http-server ''
       hej hej I'm node-1
-      ${common.info config.terranix}
+      ${common.info input}
     '') ];
   };
-  "node-2" = { config, ... }: {
+  "node-2" = { config, input, ... }: {
     require = [ (common.http-server ''
       hello my name is node-2
-      ${common.info config.terranix}
+      ${common.info input}
     '') ];
   };
 }
--- examples/network-dynamic-nodes.nix
+++ examples/network-dynamic-nodes.nix
@@ -1,10 +1,9 @@
 let
   common = import ./common.nix;
-in input: {
-  "node-1" = { config, input, ... }: {
+in builtins.mapAttrs (name: node:
+  { config, input, ... }: {
     require = [ (common.http-server ''
-      hej hej I'm node-1
+      hej hej I'm ${name} (hej = ${node.ip})
       ${common.info input}
     '') ];
-  };
-}
+  })
