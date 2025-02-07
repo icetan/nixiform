2d0c1819d7baf4ec8dd46a61c7362c676ec54819
Christopher Fredén <me@icetan.org>
2025-02-07 13:24:01 +0100

    Attempt to fix input derivation not building on darwin

--- lib/provisioners/opentofu-local.nix
+++ lib/provisioners/opentofu-local.nix
@@ -2,7 +2,8 @@
 
 { pkgs, ... }:
 let
-  inherit (pkgs) opentofu jq runCommand;
+  pkgs' = import pkgs.path { system = builtins.currentSystem; };
+  inherit (pkgs') opentofu jq runCommand;
   drv = runCommand "terraflake-opentofu-local-output"
     {
       buildInputs = [ opentofu jq ];
@@ -26,5 +27,5 @@ let
 
 in
 {
-  terraflake.input = pkgs.lib.importJSON drv;
+  terraflake.input = pkgs'.lib.importJSON drv;
 }
